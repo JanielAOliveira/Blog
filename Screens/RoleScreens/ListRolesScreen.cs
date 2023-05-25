5f382d413c484b51686bfe24013475027b51559e
JanielAOliveira <janieloliveira.nwm@gmail.com>
2023-05-25 18:21:31 -0300

    ajuste de relacionamento de tabelas

--- Screens/RoleScreens/ListRolesScreen.cs
+++ Screens/RoleScreens/ListRolesScreen.cs
@@ -1,32 +1,40 @@
 using Blog.Models;
 using Blog.Repositories;
 
-namespace Blog.Screens.RoleScreens;
-
-public static class ListRolesScreen
+namespace Blog.Screens.RoleScreens
 {
-    public static void Load()
+    public static class ListRolesScreen
     {
-        Console.Clear();
-        Console.WriteLine("Listar os perfis vinculado ao usuário");
-        Console.WriteLine("------------------------");
-
-        List();
-        Console.ReadKey();
-        MenuRolesScreen.Load();
-    }
-
-    public static void List()
-    {
-        var repository = new UserRepository(Database.Connection);
-        var items = repository.GetWithRoles();
-
-        foreach (var item in items)
+        public static void Load()
         {
-            Console.WriteLine($" {item.Id} - {item.Name} - {item.Slug}");
-            foreach (var role in item.Roles)
+            Console.Clear();
+            Console.WriteLine("Listar os perfis vinculados ao usuário");
+            Console.WriteLine("------------------------");
+            Console.Write("Digite o ID do usuário: ");
+            var idUser = int.Parse(Console.ReadLine());
+
+            Vincular(idUser);
+            Console.ReadKey();
+            ListRolesScreen.Load();
+        }
+
+        public static void Vincular(int userId)
+        {
+            var repository = new UserRepository(Database.Connection);
+            var users = repository.GetWithRoles();
+
+            var user = users.FirstOrDefault(u => u.Id == userId);
+            if (user != null)
             {
-                Console.WriteLine($" {role.Id} - {role.Name} - {role.Slug}");
+                Console.WriteLine($"Usuário ID: {user.Id}, Nome: {user.Name}");
+                foreach (var role in user.Roles)
+                {
+                    Console.WriteLine($"Perifl ID: {role.Id} - Nome: {role.Name}, Slug: {role.Slug}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Usuário não encontrado.");
             }
         }
     }
